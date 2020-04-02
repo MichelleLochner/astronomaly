dc40867070b0b6e0da5d3be7b36aa9a23792580b
Michelle <dr.michelle.lochner@gmail.com>
2020-04-02 14:03:40 +0200

    spinx-js has some weird docstring rules

--- webapp/src/components/ObjectDisplayer.js
+++ webapp/src/components/ObjectDisplayer.js
@@ -6,6 +6,13 @@ import Card from '@material-ui/core/Card';
  * Class to display arbitrary data in key pairs in a neat card
  */
 export class ObjectDisplayer extends React.PureComponent{
+    /**
+     * You have to have a docstring here even if empty or it won't compile
+     * 
+     */
+    constructor(props){
+        super(props);
+    }
     render(){
         let display_text = '';
         const keys = Object.keys(this.props.object);
