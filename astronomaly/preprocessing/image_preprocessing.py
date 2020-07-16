95f6684b5c3dbbbafb48d5e7a2285ab147cf524a
Michelle <dr.michelle.lochner@gmail.com>
2020-07-16 10:06:25 +0200

    Added gaussian window transform

--- astronomaly/preprocessing/image_preprocessing.py
+++ astronomaly/preprocessing/image_preprocessing.py
@@ -143,3 +143,40 @@ def image_transform_crop(img, new_shape=[160, 160]):
     delt_0 = (img.shape[0] - new_shape[0]) // 2
     delt_1 = (img.shape[1] - new_shape[1]) // 2
     return img[delt_0:img.shape[0] - delt_0, delt_1:img.shape[1] - delt_1]
+
+
+def image_transform_gaussian_window(img, width=2.5):
+    """
+    Applies a Gaussian window of a given width to the image. This has the
+    effect of downweighting possibly interfering objects near the edge of the
+    image. 
+
+    Parameters
+    ----------
+    img : np.ndarray
+        Input image
+    width : float, optional
+        The standard deviation of the Gaussian. The Gaussian is evaluated on a
+        grid from -5 to 5 so a width=1 corresponds to a unit Gaussian. The
+        width of the Gaussian will appear to be around 1/5 of the image, which
+        would be fairly aggressive downweighting of outlying sources.
+
+    Returns
+    -------
+    np.ndarray
+        Windowed image
+
+    """
+
+    xvals = np.linspace(-5, 5, img.shape[0])
+    yvals = np.linspace(-5, 5, img.shape[1])
+    X, Y = np.meshgrid(xvals, yvals)
+    Z = 1 / np.sqrt(width) / 2 * np.exp(-(X**2 + Y**2) / 2 / width**2)
+
+    if len(img.shape) == 2:  # Only a single channel image
+        return img * Z
+    else:
+        new_img = np.zeros_like(img)
+        for i in range(img.shape[-1]):
+            new_img[:, :, i] = img[:, :, i] * Z
+        return new_img
