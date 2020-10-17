4a9dc930b2df737f2ee22e3b508b43caeee9c099
Michelle <dr.michelle.lochner@gmail.com>
2020-10-17 11:12:28 +0200

    Fiddling settings

--- docs/conf.py
+++ docs/conf.py
@@ -30,14 +30,13 @@ def run_apidoc(_):
     cur_dir = os.path.abspath(os.path.dirname(__file__))
     output_path = os.path.join(cur_dir, 'apidocs')
     module = '../astronomaly'
-    ignore = '../astronomaly/scripts/*'
     cmd_path = 'sphinx-apidoc'
     if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
         # If we are, assemble the path manually
         cmd_path = os.path.abspath(os.path.join(sys.prefix, 
                                                 'bin', 'sphinx-apidoc'))
-    subprocess.check_call([cmd_path, '-e', '-o', output_path, 
-                           module, ignore, '--force'])
+    subprocess.check_call([cmd_path,  '-o', output_path, 
+                           module, '--force'])
 
 
 def setup(app):
