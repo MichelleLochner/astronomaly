04c12528e4a929cbadb97991f36403153ad2c2d6
Michelle <dr.michelle.lochner@gmail.com>
2020-10-21 15:11:05 +0200

    Added versioning

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 import setuptools
+import re
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+VERSIONFILE = "_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
 setuptools.setup(
     name="astronomaly",
-    version="0.0.1",
+    version=verstr,
     author="Michelle Lochner",
     author_email="dr.michelle.lochner@gmail.com",
     description="A general anomaly detection framework for Astronomical data",
-    # long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/MichelleLochner/astronomaly",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD-3 License",
         "Operating System :: OS Independent",
     ],
 )
